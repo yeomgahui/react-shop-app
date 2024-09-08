5bfe2b396fff0eec98e48020237a642599f4a71f
ykh4933 <ykh4933@smartscore.kr>
2024-09-08 16:55:16 +0900

    Loader 컴포넌트 추가

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -1,44 +1,53 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
+import LogoPath from "@/assets/colorful.svg";
+import { useRouter } from "next/navigation";
+import styles from "./Auth.module.scss";
+import Loader from "@/components/loader/Loader";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const router = useRouter();
+
+  const redirectUser = () => {
+    router.push("/");
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const loginUser = (e) => {
+    e.preventDefaut();
+    setIsLoading(true);
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
+  const signInWithGoogle = () => {
+    setIsLoading(true);
   };
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </section>
+    <>
+      {isLoading && <Loader />}
+      <section className={styles.page}>
+        <div className={styles.container}>
+          <h1 className={styles.logo}>
+            <Image priority src={LogoPath} alt="logo" />
+          </h1>
+          <form onSubmit={loginUser} className={styles.form}>
+            Input
+            <div className={styles.group}>
+              {/* 자동 로그인, 비밀번호 수정 */}
+            </div>
+            <div className={styles.buttonGroup}>
+              {/* Button */}
+              <div>{/* Button */}</div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,34 @@
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./Loader.module.scss";
+
+const Loader = ({ basic }) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible={true}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
