28c0887a1c851cdc8d2641995e8100e4b3ac20ff
yeomgahui <ykh4933@naver.com>
2024-09-18 22:30:54 +0900

    Toast Provider 필요 없는 코드 삭제

--- src/components/toastProvider/ToastProvider.jsx
+++ src/components/toastProvider/ToastProvider.jsx
@@ -1,9 +1,9 @@
 "use client";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
 const ToastProvider = () => {
-  console.log("ToastProvider");
   return <ToastContainer autoClose={2000} />;
 };
 
