349d4c295ba7c7e75413474a186fb7b5a328903d
yeomgahui <ykh4933@naver.com>
2024-10-02 13:40:10 +0900

    [Feat] Product데이터 Redux에 저장하기

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,17 +1,43 @@
 "use client";
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  selectProducts,
+  STORE_PRODUCTS,
+  GET_PRICE_RANGE,
+} from "@/redux/slice/productSlice";
 import useFetchCollection from "@/hooks/useFetchCollection";
+
+import ProductList from "./productList/ProductList";
+import ProductFilter from "./productFilter/ProductFilter";
+import Loader from "../loader/Loader";
 
 import styles from "./Product.module.scss";
 
 const Product = () => {
   const { data, isLoading } = useFetchCollection("products");
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(STORE_PRODUCTS({ products: data }));
+
+    dispatch(GET_PRICE_RANGE({ products: data }));
+  }, [data, dispatch]);
+
+  const products = useSelector(selectProducts);
   return (
-    <section className={styles.product}>
-      <aside className={styles.filter}></aside>
-      <div className={styles.content}></div>
-    </section>
+    <>
+      <section className={styles.product}>
+        <aside className={styles.filter}>
+          {isLoading ? null : <ProductFilter />}
+        </aside>
+        <div className={styles.content}></div>
+      </section>
+      {isLoading ? <Loader basic /> : <ProductList />}
+    </>
   );
 };
 
---
+++ src/components/product/productFilter/ProductFilter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductFilter = () => {
+  return (
+    <div className="product-filter">
+      <h2>Product Filter</h2>
+      {/* Add your filter controls here */}
+    </div>
+  );
+};
+
+export default ProductFilter;
