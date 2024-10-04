db077af44f3a5afe993f285be886d107356955b0
yeomgahui <ykh4933@naver.com>
2024-10-04 13:44:43 +0900

    [Feat] 상품 상세 페이지 UI 생성

--- src/app/product-details/[id]/ProductDetailsClient.jsx
+++ src/app/product-details/[id]/ProductDetailsClient.jsx
@@ -1,15 +1,26 @@
 "use client";
 
-import useFetchDocument from "@/hooks/useFetchDocument";
+import { useState } from "react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 
-import styles from "./ProductDetailsClient.module.scss";
+import Button from "@/components/button/Button";
 import Loader from "@/components/loader/Loader";
+import Divider from "@/components/divider/Divider";
+
+import useFetchDocument from "@/hooks/useFetchDocument";
+import { priceFormat } from "@/utils/priceFormat";
+
+import { Rating } from "react-simple-star-rating";
+import styles from "./ProductDetails.module.scss";
+import listCashIcon from "@/assets/list-cash-icon.png";
 
 const ProductDetailsClient = () => {
   const { id } = useParams();
 
   const { document: product } = useFetchDocument("products", id);
+
+  const [count, setCount] = useState(1);
 
   const addToCart = () => {
     console.log("Add to cart");
@@ -23,7 +34,89 @@ const ProductDetailsClient = () => {
 
   return (
     <section className={styles.product}>
-      {product === null ? <Loader /> : ""}
+      {product === null ? (
+        <Loader />
+      ) : (
+        <>
+          <div className={styles.details}>
+            <div className={styles.img}>
+              <Image
+                src={product.imageURL}
+                alt={product.name}
+                width={477}
+                height={410}
+              />
+            </div>
+            <div className={styles.contetn}>
+              <div className={styles.header}>
+                <p className={styles.brandName}>{product.brand}</p>
+                <p className={styles.productName}>{product.name}</p>
+                <div className={styles.rating}>
+                  <Rating initialValue={3} size={17} />
+                  <span className={styles.count}> 10,000</span>
+                </div>
+              </div>
+
+              <Divider />
+
+              <div className={styles.container}>
+                <p className={styles.price}>{priceFormat(product.price)}원</p>
+
+                <div className={styles.rewardCacheBadge}>
+                  <Image src={listCashIcon} alt="cash-icon" width={12} />
+                  <span>최대 {product.price / 10}원 적립</span>
+                </div>
+              </div>
+
+              <Divider space={0} />
+
+              <div className={styles.rewardCashWrapper}>
+                <div className={styles.rewardCacheSummary}>
+                  <Image src={listCashIcon} alt="cash-icon" width={15} />
+                  <p>
+                    캐시적립 혜택 <span></span> 최대
+                    <strong>{product.price / 10}원 적립</strong>
+                  </p>
+                </div>
+                <div className={styles.rewordCashPromotion}>
+                  <p>쿠페이 머니 결제 시 1% 적립</p>
+                  <p>[로켓와우 + 쿠페이 계좌이체]결제 시 2% 적립</p>
+                  <p>[로켓와우 + 쿠페이 머니]결제 시 4% 추가적립</p>
+                  <button>로켓와우 무료체험 신청하기</button>
+                </div>
+              </div>
+
+              <Divider space={0} />
+
+              <div className={styles.bottom}>
+                <p className={styles.price}>{product.price + count}원</p>
+                <div className={styles.count}>
+                  <Button
+                    onClick={() => setCount((prev) => prev - 1)}
+                    disabled={count > 1 ? false : true}
+                    secondary
+                  >
+                    -
+                  </Button>
+                  <p>
+                    <b>{count}</b>
+                  </p>
+                  <Button
+                    secondary
+                    onClick={() => setCount((prev) => prev + 1)}
+                  >
+                    {" "}
+                    +
+                  </Button>
+                  <Button onClick={() => addToCart(product)}>
+                    장바구니 담기
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </section>
   );
 };
